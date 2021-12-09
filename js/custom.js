d7654fa2d0ab72ffdf96093b8eb010403ebb25ea
00011244 <wiut11244@gmail.com>
2021-12-09 23:53:07 +0500

    Contact and feedback form

--- js/custom.js
+++ js/custom.js
@@ -4,6 +4,24 @@ function numberFormat(value) {
     return formatter.format(value);
   }
 
+  (function () {
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+  
+          form.classList.add('was-validated')
+        }, false)
+      })
+  })();
 
 // Cashback calculator
 (function () {
